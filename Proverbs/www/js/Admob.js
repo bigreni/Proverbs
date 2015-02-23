d2838c26c8a06074b47c4c4104a24d7814eeaf63
bigreni <bigreni@gmail.com>
2015-02-23 13:51:36 -0800

    AdMob test9

--- Proverbs/www/js/Admob.js
+++ Proverbs/www/js/Admob.js
@@ -32,11 +32,11 @@
             // width: integer, // valid when set adSize 'CUSTOM'
             // height: integer, // valid when set adSize 'CUSTOM'
             position: AdMob.AD_POSITION.BOTTOM_CENTER,
-            offsetTopBar: false, // avoid overlapped by status bar, for iOS7+
+            // offsetTopBar: false, // avoid overlapped by status bar, for iOS7+
             bgColor: 'black', // color name, or '#RRGGBB'
             // x: integer,		// valid when set position to 0 / POS_XY
             // y: integer,		// valid when set position to 0 / POS_XY
-            isTesting: true//, // set to true, to receiving test ad for testing purpose
+            isTesting: false//, // set to true, to receiving test ad for testing purpose
             // autoShow: true // auto show interstitial ad when loaded, set to false if prepare/show
         };
         AdMob.setOptions( defaultOptions );
@@ -62,23 +62,18 @@
     function getSelectedAdSize() {
         var i = document.getElementById("adSize").selectedIndex;
         var items = document.getElementById("adSize").options;
-        alert(items[i].value);
         return items[i].value;
     }
-
     function getSelectedPosition() {
         var i = document.getElementById("adPosition").selectedIndex;
         var items = document.getElementById("adPosition").options;
-        alert(parseInt(items[i].value));
-        return parseInt( items[i].value ); //8 - Bottom Center, 2 - Top Center, 5 - Center
+        return parseInt( items[i].value );
     }
-
     function createSelectedBanner() {
     	var overlap = document.getElementById('overlap').checked;
         var offsetTopBar = document.getElementById('offsetTopBar').checked;
-        AdMob.createBanner( {adId:admobid.banner, offsetTopBar:offsetTopBar, overlap:overlap, adSize: getSelectedAdSize(), position:getSelectedPosition()} );
+        AdMob.createBanner( {adId:admobid.banner, overlap:overlap, offsetTopBar:offsetTopBar, adSize: getSelectedAdSize(), position:getSelectedPosition()} );
     }
-
     function createBannerOfGivenSize() {
         var w = document.getElementById('w').value;
         var h = document.getElementById('h').value;
